4f697daa7a608571924ce255a90113631d0b01e3
Sebastian Lohr <sebastian.lohr@partner.digitalsolutions.com>
2022-05-04 20:23:16 +0200

    add default spring project

---
+++ src/main/java/de/doubleslash/swencloud/SwenCloudApplication.java
@@ -0,0 +1,13 @@
+package de.doubleslash.swencloud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwenCloudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwenCloudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/doubleslash/swencloud/SwenCloudApplicationTests.java
@@ -0,0 +1,13 @@
+package de.doubleslash.swencloud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwenCloudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
